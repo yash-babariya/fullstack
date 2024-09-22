6bd5526c822bc0d8c43b0bd58c550beef5bd9857
yash-babariya <yashbabariya549@gmail.com>
2024-09-22 14:31:25 +0530

    some changes...

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,27 @@
 import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import path from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import cors from 'cors';
 import apiRoutes from './routes/api.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-dotenv.config();
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5353;
 
 app.use(cors());
 app.use(express.json());
 
+// API routes
 app.use('/api', apiRoutes);
 
 // Serve static files from the React frontend app
-app.use(express.static(new URL('../../frontend/build', import.meta.url).pathname));
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-    res.sendFile(new URL('../../frontend/build/index.html', import.meta.url).pathname);
+    res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 
 app.listen(PORT, () => {
