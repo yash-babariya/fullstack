44e5bff768c5b08d962f4e49ae98ec76a81cad3e
yash-babariya <yashbabariya549@gmail.com>
2024-09-22 12:56:33 +0530

    some changes..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5353',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
+      '/api': 'http://localhost:5353'
     }
   }
 })
